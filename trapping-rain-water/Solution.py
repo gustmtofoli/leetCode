46933a63b50ce475de7e6cb26d685d37a14400ab
Gustavo <gmtofoli@gmail.com>
2019-11-11 08:14:35 -0300

    Trapping rain water

---
+++ trapping-rain-water/Solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        
+        if n <= 1:
+            return 0
+        
+        maxLeft = [None]*n
+        maxRight = [None]*n
+        
+        maxLeft[0] = height[0]
+        for i in range(1, n):
+            maxLeft[i] = max(height[i], maxLeft[i-1])
+        
+        maxRight[n-1] = height[n-1]
+        for i in range(n-2, -1, -1):
+            maxRight[i] = max(height[i], maxRight[i+1])
+        
+        count = 0
+        print(maxLeft)
+        print(maxRight)
+        for i in range(n):
+            count += min( maxLeft[i], maxRight[i]) - height[i]
+        
+        return count
+
+'''
+Approach: Dynamic programming:
+    1. Find the max heights from left to right end;
+    2. Find the max heights from right to left end;
+    3. Find the minimum between the two max heights (left to end and right to end) and substract the current height from the the value
+
+Time complexity: O(n)
+'''
